615c3b6b53a19b897f0223ee7e1b17f1edd1bdbc
Cameron <cartier_h@protonmail.com>
2023-02-01 20:35:17 -0700

    updated

--- android_embedit.py
+++ android_embedit.py
@@ -155,10 +155,11 @@ class AndroidEmbed():
                 raise Exception(str(out + err))
 
         print('[*] Signing [{0}]'.format(fp))
-        cmd = 'jarsigner -verbose -keystore {0} '.format(ks)
-        cmd += '-storepass {0} '.format(kp)
-        cmd += '-digestalg SHA1 -sigalg SHA1withRSA '
-        cmd += '{0} {1}'.format(fp, kn)
+        cmd = 'apksigner sign --ks {0} '.format(ks)
+        cmd += '--ks-pass pass:{0} '.format(kp)
+        #cmd += '-digestalg SHA1 -sigalg SHA1withRSA '
+        cmd += '{0}'.format(fp)
+        print(cmd)
         out, err = self.oscmd(cmd)
         if 'jarsigner error' in out or len(err) > 0:
             raise Exception(str(out + err))
